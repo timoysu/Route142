4e48be4f8362a9a437493078ef04d32ccd5fd5e9
Julia Menchavez <jcmenchavez@gmail.com>
2014-12-04 15:58:18 +0800

    running django app

---
+++ backend/route142/thesite/views.py
@@ -0,0 +1,6 @@
+from django.http import HttpResponse
+from django.views.generic import View
+
+class IndexView(View):
+    def get(self, request):
+        return HttpResponse('Welcome to Route142!')
