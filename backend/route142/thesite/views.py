c5c146e42d0dfb60b4e3ae6aedd939212ddb7dce
Julia Menchavez <jcmenchavez@gmail.com>
2014-12-18 14:57:04 +0800

    created info query view

--- backend/route142/thesite/views.py
+++ backend/route142/thesite/views.py
@@ -1,5 +1,24 @@
+import json
+
 from django.http import HttpResponse
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, View
+
+from models import Point
 
 class IndexView(TemplateView):
     template_name = 'index.html'
+
+
+class InfoQueryView(View):
+
+    def get(self, **kwargs):
+        query = self.request.GET['query']
+        results = Point.objects.filter(name__icontains=query)
+        results_list = []
+        for result in results:
+            result_json = {}
+            result_json['type'] = result.typ
+            result_json['coordinates'] = [result.lat, result.lon]
+            result_json['name'] = result.name
+            results_list.append(result_json)
+        return HttpResponse(json.dumps(results_list))
